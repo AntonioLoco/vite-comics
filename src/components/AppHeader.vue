89cc14fac5f7cc7890175efe1c76163a91f7a50d
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-11-08 15:43:37 +0100

    Added header and main

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,14 +2,124 @@
 export default{
     name: "AppHeader",
     data(){
-        return {}
+        return {
+            navigations: [
+                {
+                    name: "characters",
+                    href: "/characters",
+                    active: false,
+                },
+                {
+                    name: "comics",
+                    href: "/comics",
+                    active: true,
+                },                
+                {
+                    name: "movies",
+                    href: "/movies",
+                    active: false,
+                },
+                {
+                    name: "tv",
+                    href: "/tv",
+                    active: false,
+                },
+                {
+                    name: "games",
+                    href: "/games",
+                    active: false,
+                },
+                {
+                    name: "collectibles",
+                    href: "/collectibles",
+                    active: false,
+                },
+                {
+                    name: "videos",
+                    href: "/videos",
+                    active: false,
+                },
+                {
+                    name: "fans",
+                    href: "/fans",
+                    active: false,
+                },
+                {
+                    name: "news",
+                    href: "/news",
+                    active: false,
+                },
+                {
+                    name: "shop",
+                    href: "/shop",
+                    active: false,
+                }
+            ]
+        }
     }
 }
 </script>
 
 <template>
-    <h1>Header</h1>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="Logo DC Comics">
+            </div>
+            <nav class="navigation">
+                <ul>
+                    <li v-for="link in navigations" :class="{'active' : link.active}">
+                        <a :href="link.href">{{ link.name.toUpperCase() }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- /Header -->
+
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+header {
+
+    .container{
+        @include flex-center("horizontal");
+    }
+    .logo{
+        width: 30%;
+        padding: .5em 0;
+        @include flex-center("vertical");
+    }
+
+    .navigation{
+        width: 70%;
+
+        ul{
+            height: 100%;
+            @include flex-center("horizontal");
+
+            li {
+                height: 100%;
+                @include flex-center("vertical");
+                margin: 0 .5em;
+                border-bottom: 5px solid #fff;
+
+                a{
+                    color: #000;
+                    font-weight: bold;
+                    font-size: .8rem;
+                }
+
+                &.active{
+                    border-bottom: 5px solid $primary-color;
+                }
+            }
+        }
+    }
+}
 </style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,14 +2,106 @@
 export default{
     name: "ProductList",
     data(){
-        return {}
+        return {
+            navigationLinks: [
+                {
+                    name: "digital comics",
+                    href: "/digital-comics",
+                    urlImage: "../assets/img/buy-comics-digital-comics.png"
+                },
+                {
+                    name: "dc merchandise",
+                    href: "/dc-merchandise",
+                    urlImage: "../assets/img/buy-comics-merchandise.png"
+                },
+                {
+                    name: "subscription",
+                    href: "/subscription",
+                    urlImage: "../assets/img/buy-comics-subscriptions.png"
+                },
+                {
+                    name: "comic shop locator",
+                    href: "/comic-shop-locator",
+                    urlImage: "../assets/img/buy-comics-shop-locator.png"
+                },
+                {
+                    name: "dc power visa",
+                    href: "/dc-power-visa",
+                    urlImage: "../assets/img/buy-dc-power-visa.svg"
+                },
+            ]
+        }
+    },
+    methods: {
+        getImagePath(imgPath){
+            return new URL(imgPath, import.meta.url).href;
+        }
     }
 }
 </script>
 
 <template>
-    <h1>Product List</h1>
+    <main>
+        <section class="product">
+            <div class="container">
+                <div class="product-list">
+                    <h2>--> Content goes here &lt;-- </h2>
+                </div>
+            </div>
+        </section>
+
+        <section class="icons-banner">
+            <div class="container">
+                <ul>
+                    <li v-for="link in navigationLinks">
+                        <img :src="getImagePath(link.urlImage)" :alt="link.name">
+                        <a href="">{{link.name.toUpperCase()}}</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+main{
+    color: #fff;
+    .product{
+        background-color: $secondary-color;
+        .product-list{
+            padding: 2em 0;
+        }
+    }
+
+    .icons-banner{
+        background-color: $primary-color;
+
+        .container{
+            padding: 2em 0;
+
+            ul{
+                display: flex;
+                justify-content: space-between;
+
+                li{
+                    @include flex-center;
+                    img{
+                        width: 50px;
+                    }
+
+                    a{
+                        font-size: .8rem;
+                        color: #fff;
+                        padding: 0 .5em;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
 </style>
